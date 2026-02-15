4504b3e583891a8e726681314a79507180d4b83f
Anubhav Kumar Singh <aanusingh927@gmail.com>
2026-02-15 13:51:45 +0530

    Add files via upload
    Fix Indentation errors and logical structure grade calculator

---
+++ check_grade.py
@@ -0,0 +1,27 @@
+#Student Grade Calculator
+
+print("__GRADE EVALUATION SYSTEM__")
+marks=float(input("Enter your marks(0-100):"))    #taking inpput
+if marks >=90:
+ grade="A+"
+ comment="Exclelent Performance!"
+elif marks>=80:
+ grade="A"
+ comment="Grate Job!"
+elif marks>=70:
+ grade="B+"
+ comment="Good,  Keep it up and make it bigger"
+elif marks>=60:
+ grade="B"
+ comment="Satisfactory! Need more efforts"
+elif marks>=50:
+ grade="B-"
+ comment="Bad Result"
+elif 0<=marks<50:
+ grade="F"
+ comment="Failed, don't give up!"
+else:
+ grade="Invalid"
+ comment="Please enter marks between 0 and 100"
+print(f"\nGrade:{grade}")
+print(f"Teacher's Remark:{commment}")
