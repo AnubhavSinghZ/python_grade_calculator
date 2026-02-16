eb97522cbf324d536900722b8c72146c5d63a5f5
Anubhav Kumar Singh <aanusingh927@gmail.com>
2026-02-16 11:54:42 +0530

    Update grade evaluation logic and comments

--- check_grade.py
+++ check_grade.py
@@ -5,23 +5,24 @@ marks=float(input("Enter your marks(0-100):"))    #taking inpput
 if marks >=90:
  grade="A+"
  comment="Exclelent Performance!"
-elif marks>=80:
+elif marks>=75:
  grade="A"
  comment="Grate Job!"
-elif marks>=70:
+elif marks>=65:
  grade="B+"
  comment="Good,  Keep it up and make it bigger"
-elif marks>=60:
+elif marks>=55:
  grade="B"
  comment="Satisfactory! Need more efforts"
-elif marks>=50:
+elif marks>=45:
  grade="B-"
  comment="Bad Result"
-elif 0<=marks<50:
+elif 0<=marks<45:
  grade="F"
  comment="Failed, don't give up!"
 else:
  grade="Invalid"
  comment="Please enter marks between 0 and 100"
 print(f"\nGrade:{grade}")
+
 print(f"Teacher's Remark:{commment}")
