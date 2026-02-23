38a26f2126468be5eebf7c69b705faae19c205c1
Anubhav Kumar Singh <aanusingh927@gmail.com>
2026-02-23 22:38:37 +0530

    Update check_grade.py

--- check_grade.py
+++ check_grade.py
@@ -5,19 +5,19 @@ marks=float(input("Enter your marks(0-100):"))    #taking inpput
 if marks >=90:
  grade="A+"
  comment="Exclelent Performance!"
-elif marks>=75:
+elif marks>=70:
  grade="A"
  comment="Grate Job!"
-elif marks>=65:
+elif marks>=60:
  grade="B+"
  comment="Good,  Keep it up and make it bigger"
-elif marks>=55:
+elif marks>=50:
  grade="B"
- comment="Satisfactory! Need more efforts"
-elif marks>=45:
+ comment="Satisfactory!"
+elif marks>=40:
  grade="B-"
- comment="Bad Result"
-elif 0<=marks<45:
+ comment="Bad Result, DO WORK HARD!"
+elif 0<=marks<40:
  grade="F"
  comment="Failed, don't give up!"
 else:
@@ -26,3 +26,4 @@ else:
 print(f"\nGrade:{grade}")
 
 print(f"Teacher's Remark:{commment}")
+
